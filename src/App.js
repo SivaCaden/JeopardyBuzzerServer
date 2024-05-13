f74d4e2fb3039003bec3e09888a21885069a42ef
nedac27 <nedac27@gmail.com>
2024-05-13 14:35:23 -0400

    main paged updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
-import logo from './logo.svg';
+import './App.scss';
 
 function App() {
 	return (
-		<div>
-			<h1>Jeopardy Buzzer Server</h1>
+		<div classname="bunger">
+			<h1>Jeopardy Buzzer</h1>
+			<div id="inputform">
+				<input type="text" id="name" placeholder="name" />
+				<input type="text" id="code" placeholder="challenge_code" />
+				<button classname="join">Join</button>
+			</div>
+
+			<div id="endorsements">
+				<h2>Endorsements</h2>
+				<div id="endorsementList">
+					<div classname="endorsement">
+						<p>
+							Endorser: <span classname="endorser">[Redacted]</span>
+						</p>
+						<p>
+							Endorsement:{' '}
+							<span classname="endorsement">
+								i cant get enough of this buzzer please
+							</span>
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
